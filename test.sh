48eb58ef5ae33312da9be568d41dc656c14c85e0
Daniel Bustamante Ospina <daniel.bustamante@sofka.com.co>
2018-08-22 19:10:16 -0500

    test file

---
+++ test.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+mongod --replSet rs1 --logpath "1.log" --dbpath db1 --port 27017 --oplogSize 64 --fork --smallfiles
+mongod --replSet rs1 --logpath "2.log" --dbpath db2 --port 27018 --oplogSize 64 --smallfiles --fork
+mongod --replSet rs1 --logpath "3.log" --dbpath db3 --port 27019 --oplogSize 64 --smallfiles --fork
+mongod --replSet rs1 --logpath "3.log" --dbpath db4 --port 27019 --oplogSize 64 --smallfiles --fork
